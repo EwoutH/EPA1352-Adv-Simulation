b6b6156d00c16da3c5c8f70326dc0959de43956e
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2022-02-21 15:37:47 +0100

    triangular_function: Implement get_delay_value function
    Implement a function that returns a single delay value for a certain bridge length class
    
    Co-Authored-By: zoefhuizing <93313070+zoefhuizing@users.noreply.github.com>
    Co-Authored-By: CWO65 <84501201+CWO65@users.noreply.github.com>

--- EPA1352-G06-A2/model/triangular_function.py
+++ EPA1352-G06-A2/model/triangular_function.py
@@ -1,11 +1,23 @@
 import numpy as np
 from numpy.random import default_rng
+
 rng = default_rng()
 
-break_XL = rng.triangular(60, 120, 240)
+delay_dict = {
+    "XL": ("triangular", (60, 120, 240)),
+    "L": ("uniform", (45, 90)),
+    "M": ("uniform", (15, 60)),
+    "S": ("uniform", (10, 20)),
+}
 
-break_L = rng.uniform(45, 90)
 
-break_M = rng.uniform(15, 60)
+def get_delay_value(bridge_class):
+    if delay_dict[bridge_class][0] == "triangular":
+        return rng.triangular(*delay_dict[bridge_class][1])
 
-break_S = rng.uniform(10, 20)
+    if delay_dict[bridge_class][0] == "uniform":
+        return rng.uniform(*delay_dict[bridge_class][1])
+
+    else:
+        print("Unknown input!")
+        return
