fcd7aac9255ac669d6e52feae26ca2ab94192d40
Ewout ter Hoeven <e.m.terhoeven@student.tudelft.nl>
2022-03-31 07:37:32 +0200

    Rerun notebook and upload new results

--- EPA1352-G06-A4/model/model_run.py
+++ EPA1352-G06-A4/model/model_run.py
@@ -10,9 +10,9 @@ import pandas as pd
 
 # run time 5 x 24 hours; 1 tick 1 minute
 # run_length = 5 * 24 * 60
-run_length = 60 * 60
+run_length = 48 * 60
 cooldown = 24 * 60
-reps = 5
+reps = 30
 seed = 1234
 
 # Loop for all 5 scenarios - each scenario runs for 10 replications
@@ -26,6 +26,6 @@ for scenario in [4]:
         df = pd.concat([df,df1])
         print(f'Replication {rep} of scenario {scenario} finished.')
     # Finally saving to csv for each scenario
-    df.to_csv(f'../experiments/iteration/scen_{scenario}_{int(run_length/60)}_hours_{reps}_reps_{int(cooldown/60)}h_cooldown.csv')
+    df.to_csv(f'../experiments/results_scenario_{scenario}.csv')
     print(f'Scenario {scenario} has been finished.')
 print("Finished!")
